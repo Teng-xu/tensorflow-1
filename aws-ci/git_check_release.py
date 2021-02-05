ada05cd936b323a7308441bebe5ab737dde8e226
Teng Xu <xuteng@amazon.com>
2021-02-05 09:16:36 +0000

    update git sync scripts

--- aws-ci/git_check_release.py
+++ aws-ci/git_check_release.py
@@ -9,15 +9,19 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--tf_version', default=2.4,
                     help='Tenforflow Version')
 args = parser.parse_args()
-
+tf_version = args.tf_version
 
 def main():
-    tf_branch = "r{tf_version}_aws".format(tf_version=args.tf_version)
-    git_branch = "r{tf_version}".format(tf_version=args.tf_version)
+    if(tf_version == 'master'):
+        tf_branch = "development"
+        git_branch = "master"
+    else:
+        tf_branch = f"r{tf_version}_aws"
+        git_branch = f"r{tf_version}"
     os.system('git clone codecommit::us-west-2://aws-tensorflow')
     os.chdir('aws-tensorflow/')
-    os.system('git checkout {tf_branch}'.format(tf_branch=tf_branch))
-    os.system('git checkout -b r{tf_version}_aws_git'.format(tf_version=args.tf_version))
+    os.system(f'git checkout {tf_branch}')
+    os.system(f'git checkout -b {tf_branch}_git')
     os.system('git remote add google https://github.com/tensorflow/tensorflow.git')
     print('git', 'pull', '--commit', 'google', git_branch)
     output = subprocess.run(['git', 'pull', '--commit', 'google', git_branch], stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -26,10 +30,10 @@ def main():
     if ("Already up to date" in output):
         print('Branch already up to date.')
     else:
-        os.system('git push --set-upstream origin r{tf_version}_aws_git'.format(tf_version=args.tf_version))
+        os.system(f'git push --set-upstream origin {tf_branch}_git')
         os.system('aws codecommit create-pull-request --title "TF{tf_version} git sync" --description "Sync tf {tf_version} with vanilla tf branch" '
-                  '--client-request-token request_token --targets repositoryName=aws-tensorflow,sourceReference=r{tf_version}_aws_git,destinationReference={tf_branch}'
-                  .format(tf_version=args.tf_version, tf_branch=tf_branch))
+                  '--client-request-token request_token --targets repositoryName=aws-tensorflow,sourceReference={tf_branch}_git,destinationReference={tf_branch}'
+                  .format(tf_version=tf_version, tf_branch=tf_branch))
 
 
 if __name__ == '__main__':
